9cc730db05ef5810ebdf39c9c501f3224fb1daf5
Reuben Son <reubenson@gmail.com>
2023-03-27 14:33:31 -0400

    try deploying to /docs

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    // outDir: './'
+    outDir: './docs'
   },
   css: {
     postcss
