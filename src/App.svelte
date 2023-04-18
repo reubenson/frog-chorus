183089d32f25f6b826893a0dbdc2cc0790abc8f3
Reuben Son <reubenson@gmail.com>
2023-04-18 15:42:07 -0400

    push to stg

--- src/App.svelte
+++ src/App.svelte
@@ -107,7 +107,7 @@
           For an introduction to current topics around the ecology of frog choruses, check out <a href="https://www.nytimes.com/2022/04/28/science/frogs-mating-songs.html" target="_blank">Now That's What I Call Frog Mating Music</a> and <a href="https://www.nytimes.com/2023/03/27/opinion/frogs-vernal-pools-ecosystem-climate.html" target="_blank">Why Tiny Ponds and Singing Frogs Matter So Much</a>.
         </p> -->
         <p>
-          This project is dedicated to the memory of the Dutch physicist and sound artist, <a href="https://simple.wikipedia.org/wiki/Felix_Hess" target="_blank">Felix Hess</a> (1941 - 2022). To learn more the origins of this project and intent of this project, click <span class="inline-block rotate-[50deg]">&#10008</span> in the top left corner.
+          This project is dedicated to the memory of the Dutch physicist and sound artist, <a href="https://simple.wikipedia.org/wiki/Felix_Hess" target="_blank">Felix Hess</a> (1941 - 2022). To learn more the origins of this project and intent of this project, click <span class="inline-block rotate-[50deg] pl-1 pr-1">&#10008</span> in the top left corner.
         </p>
         <p>
           For a suggestion on how to use this app:
