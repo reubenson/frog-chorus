0078f7eaa81fde51951a52d1bcdf6ddf108e4d11
Reuben Son <reubenson@gmail.com>
2024-02-12 11:44:14 -0500

    update readme, tweaks to language, minor visual updates (#11)
    * update readme
    
    * various bugfixes and tweaks
    
    * rebuild stg
    
    * update language and visual tweaks
    
    * build to prod

--- src/Home.svelte
+++ src/Home.svelte
@@ -64,7 +64,7 @@
         <span class="italic">Frog Chorus</span> is an audio-based web-app that allows your mobile device or laptop to chirp in a “chorus” of other devices, as if they were a <a href="https://www.youtube.com/watch?v=aPAchkz76c8" target="_blank">chorus of frogs in the wild</a>.
       </p>
       <p>
-        This app uses your device's built-in speaker and microphone to communicate with other devices running the app, through sound only. No data is sent over the internet, and no data is stored on any server (aside from Google Analytics). While you <em>can</em> run this app alone, your frog will not make much sound unless you're in a relatively quiet environment with lots of other frogs (phones) around.
+        The app uses the built-in speaker and microphone to have devices listen to and interact with each other, generating a dynamic and spatialized work of sound sculpture. While you <em>can</em> run this app alone, your frog will not make much sound unless you're in a relatively quiet environment with lots of other frogs (phones) around.
       </p>
       <button
         class="border-black font-bold border-2 text-emerald-100 p-2 mt-10 tracking-[.2em] m-auto block font-mono h-[70px] w-[80px] border-black ring-2 bg-right-top rounded-full"
