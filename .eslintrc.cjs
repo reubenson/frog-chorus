ef62b1425d10d028b4d633fcdc62f69d2d3de631
Reuben Son <reubenson@gmail.com>
2024-08-05 15:23:23 -0400

    tweak linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'],
+    'plugin:prettier/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
